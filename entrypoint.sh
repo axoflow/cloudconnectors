295478af51543f448b22fbb8618c10f52fd7ed63
Bence Csati <bence.csati@axoflow.com>
2025-04-02 16:45:42 +0200

    feat: spit prod and dev config
    Signed-off-by: Bence Csati <bence.csati@axoflow.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,9 +19,30 @@ detect_provider() {
     echo "$provider" | xargs
 }
 
-if PROVIDER=$(detect_provider); then
-    echo "Detected ${PROVIDER} configuration"
-    exec ./axoflow-otel-collector --config "/etc/axoflow-otel-collector/connectors/common-config.yaml" --config "/etc/axoflow-otel-collector/connectors/${PROVIDER}/config.yaml"
+is_prod_config_needed() {
+    local config_type=""
+
+    if [ -n "${AXOROUTER_CA_FILE:-}" ] || \
+       [ -n "${AXOROUTER_CA_PEM:-}" ] || \
+       [ -n "${AXOROUTER_CERT_FILE:-}" ] || \
+       [ -n "${AXOROUTER_CERT_PEM:-}" ] || \
+       [ -n "${AXOROUTER_KEY_FILE:-}" ] || \
+       [ -n "${AXOROUTER_KEY_PEM:-}" ] || \
+       [ -n "${AXOROUTER_TLS_MIN_VERSION:-}" ] || \
+       [ -n "${AXOROUTER_TLS_MAX_VERSION:-}" ] || \
+       [ -n "${AXOROUTER_INCLUDE_SYSTEM_CA_CERTS_POOL:-}" ] || \
+       [ -n "${AXOROUTER_INSECURE_SKIP_VERIFY:-}" ]; then
+       config_type="prod"
+    else
+        config_type="dev"
+    fi
+
+    echo "$config_type" | xargs
+}
+
+if PROVIDER=$(detect_provider) && CONFIG_TYPE=$(is_prod_config_needed); then
+    echo "Detected ${PROVIDER} configuration with ${CONFIG_TYPE} settings."
+    exec ./axoflow-otel-collector --config "/etc/axoflow-otel-collector/connectors/common-config-${CONFIG_TYPE}.yaml" --config "/etc/axoflow-otel-collector/connectors/${PROVIDER}/config.yaml"
 fi
 
 echo "No cloud provider configuration detected. Please set environment variables for one of:"
